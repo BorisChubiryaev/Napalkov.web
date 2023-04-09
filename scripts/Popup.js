58006462dd7966207d03072c43f96b8e49dd4058
Boris Chubiryaev <boriks7@yandex.ru>
2023-04-09 21:50:58 +0300

    Correct button for horizontal scrolling

--- scripts/Popup.js
+++ scripts/Popup.js
@@ -16,15 +16,60 @@ const offPopup = function() {
 openPopup.addEventListener('click', onPopup);
 closePopup.addEventListener('click', offPopup);
 
-let hg = document.getElementsByClassName('hg');
-console.log(hg)
-let move = 0;
-function plus(){
-    move -= 210;
-    hg[0].style.marginLeft = move + 'px';  
-}
-function minus(){
-    move += 210;
-    hg[0].style.marginLeft = move + 'px';
+var totalWidth = 0;
 
-}
+$('.slider-item').each(function(index) {
+    totalWidth += parseInt($(this).outerWidth(true), 10);
+});
+
+$('.slider-win').width(totalWidth);
+
+$('#sl-next').on('click', function(){
+  
+  
+  var $this = $(this),
+      $slider = $this.closest('.slider'),
+      $sliderWin = $slider.find('.slider-win'),
+      item = $sliderWin.find('.slider-item'),
+      pos = item.outerWidth(true); 
+  
+  
+  if($sliderWin.css('margin-left') <= '-1200px'){
+    $sliderWin.animate(
+      {'margin-left': '0px'}, 
+      1000
+    ); 
+  } else {
+    $sliderWin.animate(
+      {'margin-left': '-='+pos}, 
+      1000
+    ); 
+  }
+  
+  
+});
+
+$('#sl-prev').on('click', function(){
+  
+  
+  var $this = $(this),
+      $slider = $this.closest('.slider'),
+      $sliderWin = $slider.find('.slider-win'),
+      item = $sliderWin.find('.slider-item'),
+      pos = item.outerWidth(true),
+      posX = $sliderWin.offset().left; 
+    
+    if(posX <= 0){
+      $sliderWin.animate(
+        {'margin-left': '+='+pos}, 
+        1000
+      );  
+    } else {
+      $sliderWin.animate(
+        {'margin-left': '0px'},  
+        1000
+      ); 
+    }
+    
+  
+});
