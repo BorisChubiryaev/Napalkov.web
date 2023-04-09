c51bc7ea9417ce76e593218dda1c8dd04e6d1228
Boris Chubiryaev <boriks7@yandex.ru>
2023-04-09 20:51:18 +0300

    first edit scroll btn

--- scripts/Popup.js
+++ scripts/Popup.js
@@ -1,6 +1,9 @@
 const popup = document.querySelector('.popup');
 const openPopup = document.querySelector('.rewiew');
 const closePopup = popup.querySelector('.popup__close');
+// const toRight = document.querySelector('.rigtBtn')
+// let container = document.querySelector('.container')
+
 
 const onPopup = function() {
     popup.classList.toggle('popup_opened');
@@ -13,3 +16,15 @@ const offPopup = function() {
 openPopup.addEventListener('click', onPopup);
 closePopup.addEventListener('click', offPopup);
 
+let hg = document.getElementsByClassName('hg');
+console.log(hg)
+let move = 0;
+function plus(){
+    move -= 210;
+    hg[0].style.marginLeft = move + 'px';  
+}
+function minus(){
+    move += 210;
+    hg[0].style.marginLeft = move + 'px';
+
+}
